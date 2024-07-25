1820fcc6be7d8df16b8fb47d429f134a9ef726a5
Fitti <dev@fitti.io>
2024-07-25 16:05:50 +0200

    Remove intermediary struct

--- src/main.zig
+++ src/main.zig
@@ -32,19 +32,16 @@ pub const Writer = struct {
     writeAllFn: *const fn (ptr: *anyopaque, data: []const u8) void,
 
     pub fn init(ptr: anytype) Self {
-        const T = @TypeOf(ptr);
-        const ptr_info = @typeInfo(T);
-
-        const gen = struct {
-            pub fn writeAll(pointer: *anyopaque, data: []const u8) void {
-                const self: T = @ptrCast(@alignCast(pointer));
-                @call(.always_inline, ptr_info.Pointer.child.writeAll, .{ self, data });
-            }
-        };
-
         return .{
             .ptr = ptr,
-            .writeAllFn = gen.writeAll,
+            .writeAllFn = struct {
+                const T = @TypeOf(ptr);
+                const ptr_info = @typeInfo(T);
+                pub fn writeAll(pointer: *anyopaque, data: []const u8) void {
+                    const self: T = @ptrCast(@alignCast(pointer));
+                    @call(.always_inline, ptr_info.Pointer.child.writeAll, .{ self, data });
+                }
+            }.writeAll,
         };
     }
 
